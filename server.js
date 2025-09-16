d60062d16cd895a1a54e7eaed13e5367b62c7f2c
coolbulldj <matthewjcdt@gmail.com>
2025-09-16 11:10:47 +1000

    work

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const Express = require('express');
-const httpProxy = require('http-proxy');
-const proxy = httpProxy.createProxyServer({ changeOrigin: true });
+const { create_proxy_middleware } = require('http-proxy-middleware');
 const app = Express();
 
 
-app.get('/', (req, res) => {
+
+app.get('/', (req, res, next) => {
     const targetUrl = req.query.url
-   
-    proxy.web(req, res, { target: targetUrl, followRedirects:false }, (err) => {
-        res.status(500).send(`Proxy error: ${err.message}`);
-    });
+    
+    const middleware = create_proxy_middleware({
+        target: targetUrl,
+        changeOrigin: true,
+        selfHandleResponse: true,
+        onProxyRes(proxyRes, req, res) {
+            let body = '';
+            proxyRes.on('data', chunk => body += chunk);
+            proxyRes.on('end', () => {
+                // Rewrite absolute URLs in the response body
+                const proxyBase = `${req.protocol}://${req.headers.host}/?url=${targetUrl}`;
+                const rewritten = body.replace(new RegExp(targetUrl, 'g'), proxyBase);
+                res.end(rewritten);
+            });
+        }
+    })
+    middleware(req, res, next);
 })
 
 //Shutdown Processes
